9918e3e103e9f1de3c27649d7b53616b37c95541
muhammad shoaib <muhammadshoaib0k0@gmail.com>
2023-03-13 21:47:11 +0500

    today done array frequency each element with 3 approach

--- app/interview/test.rb
+++ app/interview/test.rb
@@ -1,7 +1,7 @@
 
 # string = "Hello apple pie p" its return -1 also because both "hello" & "apple" have same num of repeating element .
 # string = "hello pie" return the keyword pie 
-
+# Question # 1
 def word_repeat(str)
     arr= []
     if str.split(" ").max_by { |s| s.length - s.chars.uniq.length} !=  str.split(" ").reverse.max_by { |s| s.length - s.chars.uniq.length}
@@ -12,3 +12,20 @@ def word_repeat(str)
 end
 
 puts word_repeat(string)
+
+
+# Question # 2
+
+array = [5,5,5,4,3,2,4,3,2,1]
+result = Hash[array.uniq.map{ |v| [v, array.count(v)]}]
+puts result
+
+puts array.tally
+
+def score( array )
+  hash = Hash.new(0)
+  array.each{|key| hash[key] += 1}
+  hash
+end
+
+score(array)
