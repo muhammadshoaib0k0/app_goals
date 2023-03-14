a4ea0054e88d561cf455e7a7955586827bbf2d37
muhammad shoaib <muhammadshoaib0k0@gmail.com>
2023-03-14 21:50:57 +0500

    add question 3 find duplicate

--- app/interview/test.rb
+++ app/interview/test.rb
@@ -29,3 +29,15 @@ def score( array )
 end
 
 score(array)
+
+# Question # 3
+
+def check_dup(arr)
+  if arr.max_by { |i| arr.count(i) } != arr.reverse.max_by { |i| arr.count(i) }
+    return -1
+  else
+    arr.max_by { |i| arr.count(i) }
+  end
+end
+
+puts check_dup([1,2,2,3,4,4])
