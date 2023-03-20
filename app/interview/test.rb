f10fd51faded6fe37f53926252332e5a347e3b3a
muhammad shoaib <muhammadshoaib0k0@gmail.com>
2023-03-20 23:19:53 +0500

    add question no 5

--- app/interview/test.rb
+++ app/interview/test.rb
@@ -58,3 +58,11 @@ def recur_flatten(array, results=[])
 end
 
 puts recur_flatten(array)
+
+# Question # 5
+
+h = {"a"=>1, "c"=>3, "b"=>2, "d"=>4}
+puts Hash[h.sort]
+hsh ={"a" => 1000, "b" => 10, "c" => 200000}
+puts Hash[hsh.sort_by{|k,v| v}]
+
