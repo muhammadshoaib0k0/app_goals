c525da7c5595be76aceefe0eb8258c8b0344dafb
muhammad shoaib <muhammadshoaib0k0@gmail.com>
2023-04-04 23:56:06 +0500

    resolve rubocop

--- app/models/comparison.rb
+++ app/models/comparison.rb
@@ -1,8 +1,8 @@
 class Comparison < ApplicationRecord
-   belongs_to :superior_movie, class_name: "Movie"
-   belongs_to :inferior_movie, class_name: "Movie"
-   # Triple-Join after addming user_id in comparison
-   # This table displays two users with opposite opinions.
-   # User 1 creates the same hierarchy with which we’ve been working (movie 1 is superior to 3, which is superior to 2). User 2 has the reverse opinion: they think that movie 1 is inferior to movie 3, which is inferior to movie 2.
-   belongs_to :user, class_name: "User"
+  belongs_to :superior_movie, class_name: 'Movie'
+  belongs_to :inferior_movie, class_name: 'Movie'
+  # Triple-Join after addming user_id in comparison
+  # This table displays two users with opposite opinions.
+  # User 1 creates the same hierarchy with which we’ve been working (movie 1 is superior to 3, which is superior to 2). User 2 has the reverse opinion: they think that movie 1 is inferior to movie 3, which is inferior to movie 2.
+  belongs_to :user, class_name: 'User'
 end
