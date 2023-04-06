68faa183b348c568432801a1cdd58c7c049a80af
muhammad shoaib <muhammadshoaib0k0@gmail.com>
2023-04-06 23:00:37 +0500

    assesment

--- app/interview/test.rb
+++ app/interview/test.rb
@@ -99,3 +99,52 @@ end
 
 # Example usage
 puts reverse_string("hello, world!") # prints "!dlrow ,olleh" 
+
+#  Submission task
+
+# Exercise:
+
+# Complete the given Bidding class that will be used for a bidding system.
+
+class Agent
+  # This will already be implemented and is not part of this exercise
+  def initialize; end
+
+  # This will return the amount (integer) by which the agent wants to increase its bid
+  # (i.e. how much they want to add onto their bid as it stands so far)
+  # This will already be implemented and is not part of this exercise
+  def get_bid_increase; end
+end
+
+
+class Bidding
+  def initialize(agents)
+    @agents = agents
+    # TODO: any other initialisation you need
+  end
+
+  def run
+    # TODO
+  end
+
+  # TODO (optional): add any other methods (with implementation) you find useful
+end
+
+#   Implement the sections of code marked TODO above.
+
+#   Rules for bidding:
+#   Agents always bid in sequence, e.g. agent 1 bids first, then agent 2, etc.
+#   The bidding starts with the first agent deciding on their initial bid (which will be returned by `get_bid_increase`). The amount can also be 0.
+#   The next agent (e.g. agent 2) must then increase their bid so that their bid is as much as any other agent's bid (which so far is only agent 1), or be forced to withdraw from the bidding completely. (if the agent has withdrawn, they will be skipped from here onwards). They may also decide to bid more, in which case the next agent has to match that higher bid (or withdraw).
+#   The bidding will come around to agent 1 again if their current bid does not match the highest bid. The agent must increase their bid (by returning the value they want to increase it by from `getBidIncrease`) so that their bid will match the highest bid that any other agent has put in so far, or withdraw from the bidding. They can also have the option to decide to bid more, same as discussed above.
+#   Keep in mind that the bidding can circle the group of agents more than once, depending on if agents up their bids. Each agent who wishes to proceed is required to have committed a bid that is equal to the highest bid made by any other agent, counted over the duration of the bidding process. NB: The bidding will end immediately once this condition is met - an agent is not allowed to up their bid once everyone else has matched them.
+#   There can be multiple agents left at the end of bidding process, all with the same bid. That will be handled by a second bidding process that's outside the scope of this exercise.
+
+#   Hints:
+# - Be sure you understand the above bidding rules clearly.
+# - Do not implement a literal solution to the problem, i.e. don't read each line on its own and write code just for that part of the requirements. Rather, think about it holistically so you can come up with a solution that meets all the requirements.
+# - No consideration should be paid to performance - clear, readable code is more imporant in this exercise.
+
+
+
+
