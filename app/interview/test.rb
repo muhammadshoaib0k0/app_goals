0b566be93e42c48b765caeb6bc45c551c6d40dcf
muhammad shoaib <muhammadshoaib0k0@gmail.com>
2023-05-09 19:48:29 +0500

    string test task

--- app/interview/test.rb
+++ app/interview/test.rb
@@ -101,6 +101,33 @@ end
 # Example usage
 puts reverse_string("hello, world!") # prints "!dlrow ,olleh" 
 
+# Question # 7
+str = "This is a string"
+counts = Hash.new(0)
+str.each_char { |char| counts[char] += 1 }
+puts count
+
+# Question # 8 #return maximum repeating element in string
+str = "Thissdfisadfdg0989string" 
+def max_occuring_char(str)
+  char_count = Hash.new(0)
+  
+  str.each_char do |char|
+    char_count[char] += 1
+  end
+  
+  max_char = ''
+  max_count = 0
+  
+  char_count.each do |char, count|
+    if count > max_count
+      max_char = char
+      max_count = count
+    end
+  end
+  
+  max_char
+end
 #  Submission task
 
 # Exercise:
