b8d317e90ff9a2d88438e741d53b4cb6a134e016
muhammad shoaib <muhammadshoaib0k0@gmail.com>
2023-06-15 14:32:20 +0500

    todo api with customhook

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
   post 'tasks/:id/toggle', to: 'tasks#toggle'
+
+  namespace :api do
+    namespace :v1 do
+      post 'add-todo', to: 'todo#add_todo'
+      get 'get-todo', to: 'todo#get_todo'
+      delete 'delete/:id', to: 'todo#destroy_todo'
+      patch 'update/:id', to:  'todo#update_todo'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
