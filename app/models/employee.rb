b8de8336e6d8bef1ba5e4102280a0166529525fe
Shoaib <81776895+muhammadshoaib0k0@users.noreply.github.com>
2023-09-20 11:38:09 +0500

    Revert "resolve rubocop and also has its association"

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,5 @@
 class Employee < ApplicationRecord
-  # Example: A manager has many employees, but… a manager is also an employee! self join
-  has_many :subordinates, class_name: :Employee, foreign_key: :manager_id
-  belongs_to :manager, class_name: :Employee, optional: true
+	# Example: A manager has many employees, but… a manager is also an employee! self join
+	has_many :subordinates, class_name: :Employee, foreign_key: :manager_id
+  belongs_to :manager, class_name: :Employee , optional: true 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :comparisons
   has_many :superior_movies, through: :comparisons
-  has_many :inferior_movies, through: :comparisons
+  has_many :inferior_movies, through: :comparisons 
 
   validates :name, presence: true
   # validates :email, presence: true
