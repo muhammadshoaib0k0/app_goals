39109ab332dc21e27703a7388f2e60fb7f1c9ac3
muhammad shoaib <muhammadshoaib0k0@gmail.com>
2023-10-24 22:36:57 +0500

    flattern array

--- app/interview/ruby-programs/test.rb
+++ app/interview/ruby-programs/test.rb
@@ -47,21 +47,7 @@ end
 puts check_dup([1,2,2,3,4,4]) # return -1
 puts check_dup([1,2,2,3,3,3,3,4,4]) #return 3
 
-# Question # 4
 
-array = [1,24,0,[3,5,6],7]
-def recur_flatten(array, results=[])
-  array.each do |ele|
-    if ele.class == Array
-      recur_flatten(ele, results)
-    else
-      results << ele  
-    end
-  end
-  results
-end
-
-puts recur_flatten(array)
 
 # Question # 5
 
