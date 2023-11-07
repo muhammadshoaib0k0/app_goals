e81e99e50b0c06b3b898b4a2dd47ee6d9801e75f
muhammad shoaib <muhammadshoaib0k0@gmail.com>
2023-11-07 20:56:40 +0500

    tally array

---
+++ app/interview/ruby-programs/array_tally.rb
@@ -0,0 +1,13 @@
+array = [5,5,5,4,3,2,4,3,2,1]
+result = Hash[array.uniq.map{ |v| [v, array.count(v)]}]
+puts result
+
+puts array.tally
+
+def score( array )
+  hash = Hash.new(0)
+  array.each{|key| hash[key] += 1}
+  hash
+end
+
+score(array)
