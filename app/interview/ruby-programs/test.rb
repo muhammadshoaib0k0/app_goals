ff3d6dc37fe2081255efe9c3dcf99ba076109aa7
muhammad shoaib <muhammadshoaib0k0@gmail.com>
2023-11-08 21:38:50 +0500

    word_repeats in invoze

--- app/interview/ruby-programs/test.rb
+++ app/interview/ruby-programs/test.rb
@@ -3,20 +3,6 @@
 # https://www.youtube.com/watch?v=XzM6MwBJyDE
 # https://www.ideamotive.co/ruby-on-rails/interview
 # https://dev.to/isalevine/let-s-use-rails-partial-views-to-render-art-from-magic-the-gathering-cards-5d8l
-# string = "Hello apple pie p" its return -1 also because both "hello" & "apple" have same num of repeating element .
-# string = "hello pie" return the keyword pie 
-# Question # 1
-def word_repeat(str)
-  arr= []
-  if str.split(" ").max_by { |s| s.length - s.chars.uniq.length} !=  str.split(" ").reverse.max_by { |s| s.length - s.chars.uniq.length}
-    return -1
-  else
-    str.split(" ").max_by { |s| s.length - s.chars.uniq.length}
-  end
-end
-
-puts word_repeat(string)
-
 
 
 # Question # 7
---
+++ app/interview/ruby-programs/word_repeat.rb
@@ -0,0 +1,13 @@
+# string = "Hello apple pie p" its return -1 also because both "hello" & "apple" have same num of repeating element .
+# string = "hello pie" return the keyword pie 
+# Question # 1
+def word_repeat(str)
+  arr= []
+  if str.split(" ").max_by { |s| s.length - s.chars.uniq.length} !=  str.split(" ").reverse.max_by { |s| s.length - s.chars.uniq.length}
+    return -1
+  else
+    str.split(" ").max_by { |s| s.length - s.chars.uniq.length}
+  end
+end
+
+puts word_repeat(string)
