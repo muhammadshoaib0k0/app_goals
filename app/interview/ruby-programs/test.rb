f626fdeacd2045e39f6f0d4cbbd5b5ff788be7d8
muhammad shoaib <muhammadshoaib0k0@gmail.com>
2023-11-13 20:16:58 +0500

    maximum string count

--- app/interview/ruby-programs/test.rb
+++ app/interview/ruby-programs/test.rb
@@ -4,27 +4,6 @@
 # https://www.ideamotive.co/ruby-on-rails/interview
 # https://dev.to/isalevine/let-s-use-rails-partial-views-to-render-art-from-magic-the-gathering-cards-5d8l
 
-# Question # 8 #return maximum repeating element in string
-str = "Thissdfisadfdg0989string" 
-def max_occuring_char(str)
-  char_count = Hash.new(0)
-  
-  str.each_char do |char|
-    char_count[char] += 1
-  end
-  
-  max_char = ''
-  max_count = 0
-  
-  char_count.each do |char, count|
-    if count > max_count
-      max_char = char
-      max_count = count
-    end
-  end
-  
-  max_char
-end
 #  Submission task
 
 # Exercise:
