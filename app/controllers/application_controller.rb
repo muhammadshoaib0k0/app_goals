e09349895657a4637563fb6aef85a33c05d52777
muhammad shoaib <muhammadshoaib0k0@gmail.com>
2024-03-15 23:22:52 +0500

    check api type and json type response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+    def json_request?
+        request.format.json?
+    end
+
+    def api_namespace_request?
+        request.original_fullpath.start_with?('/api/')
+    end
 end
